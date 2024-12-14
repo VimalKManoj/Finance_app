1e0f19196dbd20a0be6adcb22e06663d43dd2043
VimalKManoj <vimalkmanoj@gmail.com>
2024-12-14 22:44:22 +0530

    hono learning and setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>Protected Route</div>;
+  return (
+    <div>
+      Protected Route
+      <UserButton />
+    </div>
+  );
 }
